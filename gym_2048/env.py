ab1e07ab4a4831bc6633c9f3188a35777d6b7576
h0rban <jake.horban@gmail.com>
2022-12-04 14:01:35 -0500

    add formatting + num invalid moves + update step logic

--- gym_2048/env.py
+++ gym_2048/env.py
@@ -6,17 +6,10 @@ import matplotlib.pyplot as plt
 
 
 class Base2048Env(gym.Env):
-  metadata = {
-    'render.modes': ['human'],
-  }
-
   # NOTE: Don't modify these numbers as they define the number of
   # anti-clockwise rotations before applying the left action on a grid
 
-  LEFT = 0
-  UP = 1
-  RIGHT = 2
-  DOWN = 3
+  LEFT, UP, RIGHT, DOWN = range(3)
 
   ACTION_STRING = {
     LEFT: 'left',
@@ -26,27 +19,22 @@ class Base2048Env(gym.Env):
   }
 
   def __init__(self, width=4, height=4, max_invalid_moves=16):
-    self.width = width
-    self.height = height
 
-    self.max_power = 1 + self.width * self.height
+    if width < 1 or height < 1:
+      raise ValueError('expecting width and height to be positive')
 
-    self.observation_space = spaces.Box(
-      # low=2,
-      # high=2 ** (1 + self.width * self.height),
-      low=0,
-      high=self.max_power,
-      shape=(self.width, self.height),
-      dtype=np.int64
-    )
+    if max_invalid_moves < 1:
+      raise ValueError('expecting number of invalid moves to be positive')
+
+    self.shape = (width, height)
+    self.max_power = 1 + width * height
+    self.max_invalid_moves = max_invalid_moves
+
+    self.observation_space = spaces.Box(low=0, high=self.max_power, shape=self.shape, dtype=np.int64)
     self.action_space = spaces.Discrete(4)
 
     # Internal Variables
-    self.board = None
-    self.np_random = None
-    self.score = 0
-    self.n_invalid_moves = 0
-    self.max_invalid_moves = max_invalid_moves
+    self.board, self.np_random, self.score, self.n_invalid_moves = [None] * 4
 
     self.seed()
     self.reset()
@@ -61,19 +49,17 @@ class Base2048Env(gym.Env):
     # Align board action with left action
     rotated_obs = np.rot90(self.board, k=action)
     reward, updated_obs = self._slide_left_and_merge(rotated_obs)
-    self.board = np.rot90(updated_obs, k=4 - action)
 
     # check if the move was valid
-    if (self.board == updated_obs).all():
+    if (rotated_obs == updated_obs).all():
       self.n_invalid_moves += 1
     else:
       self.n_invalid_moves = 0
-
-      # Place one random tile on empty location
+      self.score += reward
+      self.board = np.rot90(updated_obs, k=4 - action)
       self._place_random_tiles(self.board, count=1)
 
     done = self.is_done()
-
     return self.board, reward, done, {}
 
   def is_done(self):
@@ -98,7 +84,8 @@ class Base2048Env(gym.Env):
   def reset(self, **kwargs):
     """Place 2 tiles on empty board."""
 
-    self.board = np.zeros((self.width, self.height), dtype=np.int64)
+    self.score, self.n_invalid_moves = 0, 0
+    self.board = np.zeros(self.shape, dtype=np.int64)
     self._place_random_tiles(self.board, count=2)
 
     return self.board
@@ -107,27 +94,17 @@ class Base2048Env(gym.Env):
     # raise board to power of 2, make zeros nan
     board = np.array(2 ** self.board, dtype=np.float64)
     board[self.board == 0] = np.nan
-
     return board
 
   def _sample_tiles(self, count=1):
     """Sample tile 2 or 4."""
-
-    # choices = [2, 4]
-    choices = [1, 2]
-    probs = [0.9, 0.1]
-
-    tiles = self.np_random.choice(choices, size=count, p=probs)
-    return tiles.tolist()
+    return self.np_random.choice([1, 2], size=count, p=[0.9, 0.1]).tolist()
 
   def _sample_tile_locations(self, board, count=1):
     """Sample grid locations with no tile."""
-
     zero_locs = np.argwhere(board == 0)
     zero_indices = self.np_random.choice(len(zero_locs), size=count)
-
-    zero_pos = zero_locs[zero_indices]
-    return zero_pos.tolist()
+    return zero_locs[zero_indices].tolist()
 
   def _place_random_tiles(self, board, count=1):
     if not board.all():
@@ -139,41 +116,37 @@ class Base2048Env(gym.Env):
   def _slide_left_and_merge(self, board):
     """Slide tiles on a grid to the left and merge."""
 
-    result = []
-
-    score = 0
+    score, result = 0, []
     for row in board:
       row = np.extract(row > 0, row)
       score_, result_row = self._try_merge(row)
       score += score_
-      row = np.pad(np.array(result_row), (0, self.width - len(result_row)),
-                   'constant', constant_values=(0,))
+      row = np.pad(np.array(result_row), (0, self.shape[0] - len(result_row)), 'constant', constant_values=(0,))
       result.append(row)
 
     return score, np.array(result, dtype=np.int64)
 
-  def _try_merge(self, row):
-    score = 0
-    result_row = []
+  @staticmethod
+  def _try_merge(row):
+    score, result_row = 0, []
 
     i = 1
     while i < len(row):
-      if row[i] == row[i - 1]:
-        score += (row[i] + row[i - 1]) ** 2
-        result_row.append(row[i] + 1)
+      a, b = row[i], row[i - 1]
+      if a == b:
+        score += 2 ** (a + 1)
+        result_row.append(a + 1)
         i += 2
       else:
-        result_row.append(row[i - 1])
+        result_row.append(b)
         i += 1
 
     if i == len(row):
       result_row.append(row[i - 1])
 
-    self.score += score
-
     return score, result_row
 
-  def render(self):
+  def render(self, show: bool = False):
 
     # import matplotlib.colors as mcolors
     # cmap, norm = mcolors.from_levels_and_colors(
@@ -184,7 +157,7 @@ class Base2048Env(gym.Env):
     board = self.get_board()
 
     fig, ax = plt.subplots(figsize=(4, 4))
-    im = ax.imshow(board, cmap='viridis')
+    ax.imshow(board, cmap='viridis')
 
     plt.title(f'Score: {self.score}')
 
@@ -204,8 +177,11 @@ class Base2048Env(gym.Env):
     for i in range(width):
       for j in range(height):
         if not np.isnan(board[i, j]):
-          # todo need to scale to fit the tile
           ax.text(j, i, int(board[i, j]), ha="center", va="center", color="w", fontsize=12, fontweight='bold')
 
     plt.tight_layout()
-    plt.show()
+
+    if show:
+      plt.show()
+
+    return fig, ax
