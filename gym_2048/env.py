3bec1b553b37926c05083d9814a05874e7112b85
h0rban <jake.horban@gmail.com>
2022-12-04 12:28:36 -0500

    add render + check if move was valid before adding a random tile

--- gym_2048/env.py
+++ gym_2048/env.py
@@ -1,7 +1,8 @@
-import numpy as np
 import gym
+import numpy as np
 import gym.spaces as spaces
 from gym.utils import seeding
+import matplotlib.pyplot as plt
 
 
 class Base2048Env(gym.Env):
@@ -24,13 +25,17 @@ class Base2048Env(gym.Env):
     DOWN: 'down',
   }
 
-  def __init__(self, width=4, height=4):
+  def __init__(self, width=4, height=4, max_invalid_moves=16):
     self.width = width
     self.height = height
 
+    self.max_power = 1 + self.width * self.height
+
     self.observation_space = spaces.Box(
-      low=2,
-      high=2 ** (1 + self.width * self.height),
+      # low=2,
+      # high=2 ** (1 + self.width * self.height),
+      low=0,
+      high=self.max_power,
       shape=(self.width, self.height),
       dtype=np.int64
     )
@@ -39,6 +44,9 @@ class Base2048Env(gym.Env):
     # Internal Variables
     self.board = None
     self.np_random = None
+    self.score = 0
+    self.n_invalid_moves = 0
+    self.max_invalid_moves = max_invalid_moves
 
     self.seed()
     self.reset()
@@ -55,14 +63,25 @@ class Base2048Env(gym.Env):
     reward, updated_obs = self._slide_left_and_merge(rotated_obs)
     self.board = np.rot90(updated_obs, k=4 - action)
 
-    # Place one random tile on empty location
-    self._place_random_tiles(self.board, count=1)
+    # check if the move was valid
+    if (self.board == updated_obs).all():
+      self.n_invalid_moves += 1
+    else:
+      self.n_invalid_moves = 0
+
+      # Place one random tile on empty location
+      self._place_random_tiles(self.board, count=1)
 
     done = self.is_done()
 
     return self.board, reward, done, {}
 
   def is_done(self):
+
+    # check if invalid moves are more than max_invalid_moves
+    if self.n_invalid_moves >= self.max_invalid_moves:
+      return True
+
     copy_board = self.board.copy()
 
     if not copy_board.all():
@@ -84,15 +103,18 @@ class Base2048Env(gym.Env):
 
     return self.board
 
-  def render(self, mode='human'):
-    if mode == 'human':
-      for row in self.board.tolist():
-        print(' \t'.join(map(str, row)))
+  def get_board(self):
+    # raise board to power of 2, make zeros nan
+    board = np.array(2 ** self.board, dtype=np.float64)
+    board[self.board == 0] = np.nan
+
+    return board
 
   def _sample_tiles(self, count=1):
     """Sample tile 2 or 4."""
 
-    choices = [2, 4]
+    # choices = [2, 4]
+    choices = [1, 2]
     probs = [0.9, 0.1]
 
     tiles = self.np_random.choice(choices, size=count, p=probs)
@@ -130,16 +152,15 @@ class Base2048Env(gym.Env):
 
     return score, np.array(result, dtype=np.int64)
 
-  @staticmethod
-  def _try_merge(row):
+  def _try_merge(self, row):
     score = 0
     result_row = []
 
     i = 1
     while i < len(row):
       if row[i] == row[i - 1]:
-        score += row[i] + row[i - 1]
-        result_row.append(row[i] + row[i - 1])
+        score += (row[i] + row[i - 1]) ** 2
+        result_row.append(row[i] + 1)
         i += 2
       else:
         result_row.append(row[i - 1])
@@ -148,4 +169,43 @@ class Base2048Env(gym.Env):
     if i == len(row):
       result_row.append(row[i - 1])
 
+    self.score += score
+
     return score, result_row
+
+  def render(self):
+
+    # import matplotlib.colors as mcolors
+    # cmap, norm = mcolors.from_levels_and_colors(
+    #   2 ** np.arange(1, self.max_power + 1), [
+    #     'red', 'green', 'blue' ...
+    #   ])
+
+    board = self.get_board()
+
+    fig, ax = plt.subplots(figsize=(4, 4))
+    im = ax.imshow(board, cmap='viridis')
+
+    plt.title(f'Score: {self.score}')
+
+    plt.tick_params(
+      bottom=False,
+      labelbottom=False,
+      left=False,
+      labelleft=False,
+    )
+
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['bottom'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+
+    width, height = board.shape
+    for i in range(width):
+      for j in range(height):
+        if not np.isnan(board[i, j]):
+          # todo need to scale to fit the tile
+          ax.text(j, i, int(board[i, j]), ha="center", va="center", color="w", fontsize=12, fontweight='bold')
+
+    plt.tight_layout()
+    plt.show()
